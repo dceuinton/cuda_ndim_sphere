5947eb8da1072da960754abe7e9b687ef724fc44
EUINTON <s14026002@IT046599.massey.ac.nz>
2018-11-01 12:04:18 +1300

    Got a working kernel with bugs. Need to debug for testCase 1 and 2.

--- a5.cu
+++ a5.cu
@@ -68,8 +68,50 @@ void runSequentialTestCases(long *dimensions, double* radii) {
 	print("Sequential Tests Over ----------\n");
 }
 
-__global__ void gpuCountPoints(long nPointsToTest, long nDimensions, double radius, long base) {
+__device__ void convert(long point, long base, long* index, long nDimensions) {
+	// Ensure array initialised
+	for (int i = 0; i < nDimensions; ++i) {
+		index[i] = 0;
+	}
 
+	long i = 0;
+	while (point != 0) {
+		long remainder = point % base;
+		point = point / base;
+		index[i] = remainder;
+		i++;
+	}
+}
+
+__device__ long getDimensionalValue(long point, long base, long dimension) {
+	long result = 0;
+	for (int i = 0; i < dimension; i++) {
+		result = point % base;
+		point = point / base;
+	}
+	return result; 
+} 
+
+__device__ int outside(long value, long halfBase, double radiusSquared) {
+	long difference = value - halfBase;
+	
+	if (difference * difference < radiusSquared) {
+		return 0;
+	} else {
+		return 1;	
+	}	
+}
+
+__global__ void gpuCountPoints(long nPointsToTest, double radiusSquared, long halfBase, long base, int* outsideRecord) {
+
+	long id = blockIdx.x * blockDim.x + threadIdx.x;
+	long dimension = blockDim.y;
+
+	if (id < nPointsToTest) {
+		long dimensionalValue = getDimensionalValue(id, base, dimension);
+		int isOutside = outside(dimensionalValue, halfBase, radiusSquared);
+		atomicAdd(&outsideRecord[id], isOutside);
+	}
 }
 
 int main(int argc, char** argv) {
@@ -84,11 +126,45 @@ int main(int argc, char** argv) {
 	const long halfBase = static_cast<long>(floor(radii[testCase]));
 	const long base = 2 * halfBase + 1;
 	const long nPointsToTest = powerLong(base, dimensions[testCase]);
+	const double radiusSquared = radii[testCase] * radii[testCase];
 
 	debug("gpu settings tc:%d: nPointsToTest: %ld, nDimensions: %ld, radius: %f, base: %ld", 
 		testCase, nPointsToTest, dimensions[testCase], radii[testCase], base);
 
-	
+	int nBytes = sizeof(int) * nPointsToTest;
+
+	int* outsideRecord = (int *)malloc(nBytes);
+	for (int i = 0; i < nPointsToTest; ++i) {
+		outsideRecord[i] = 0;
+	}
+
+	int* gpuOutsideRecord;
+	cudaMalloc(&gpuOutsideRecord, nBytes);
+	cudaMemcpy(gpuOutsideRecord, outsideRecord, nBytes, cudaMemcpyHostToDevice);
+
+	int nThreads = 32 * dimensions[testCase];
+	int nBlocks = (nPointsToTest + nThreads - 1) / nThreads;
+
+	dim3 blockDimensions(nThreads, dimensions[testCase], 1);
+	dim3 gridDimensions(nBlocks, 1, 1);
+
+	gpuCountPoints<<<gridDimensions, blockDimensions>>>(nPointsToTest, radiusSquared, halfBase, base, gpuOutsideRecord);
+
+	cudaMemcpy(outsideRecord, gpuOutsideRecord, nBytes, cudaMemcpyDeviceToHost);
+
+	cudaFree(gpuOutsideRecord);
+
+	int count = 0;
+	for (int i = 0; i < nPointsToTest; ++i) {
+		if (outsideRecord[i] == 0) {
+			count++;
+		}
+	}
+
+	free(outsideRecord);
+
+	print("Parallel test %d:", testCase);
+	printTestResults(dimensions[testCase], radii[testCase], count);
 	
 	return 0;
 }
