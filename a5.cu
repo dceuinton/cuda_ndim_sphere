488aee6b77a9961bc5b32edef836c52567f3477b
EUINTON <s14026002@IT046599.massey.ac.nz>
2018-11-02 09:30:52 +1300

    Got a working version. Could still optimise like Dan recommended.

--- a5.cu
+++ a5.cu
@@ -5,6 +5,8 @@ using namespace std;
 
 typedef unsigned long long ULL;
 
+const int MAX_DIMENSIONS = 10;
+
 template <typename T>
 void printArray(T* ar, int size) {
 	cout << "Printing array:" << endl;
@@ -70,7 +72,7 @@ long countPoints(long nDimensions, double radius) {
 	return count;
 }
 
-void runSequentialTestCases(long *dimensions, double* radii) {
+void runSequentialTestCases(ULL *dimensions, double* radii) {
 	print("Sequential Tests ---------------");
 	for (int i = 0; i < 3; i++) {
 		long totalPoints = countPoints(dimensions[i], radii[i]);
@@ -94,16 +96,6 @@ __device__ void convert(long point, long base, long* index, long nDimensions) {
 	}
 }
 
-// __device__ int outside(long value, long halfBase, double radiusSquared) {
-// 	long difference = value - halfBase;
-
-// 	if (difference * difference < radiusSquared) {
-// 		return 0;
-// 	} else {
-// 		return 1;	
-// 	}		
-// }
-
 __device__ ULL getDimensionalValue(ULL point, ULL base, ULL dimension) {
 	ULL result = 0;
 	for (int i = 0; i < dimension; i++) {
@@ -115,12 +107,12 @@ __device__ ULL getDimensionalValue(ULL point, ULL base, ULL dimension) {
 
 __device__ void determineOutside(ULL id, ULL dimension, 
 								 ULL* pointsLength, double radiusSquared, 
-								 int* outsideRecord) {
+								 int* record) {
 	if (dimension == 1) {
 		if (pointsLength[id] < radiusSquared) {
-			outsideRecord[id] = 0;
+			record[id] = 0;
 		} else {
-			outsideRecord[id] = 1;
+			record[id] = 1;
 		}
 	}
 }
@@ -135,36 +127,49 @@ __device__ void determineOutside(ULL id, ULL dimension,
 
 __global__ void gpuCountPoints(ULL nPointsToTest, double radiusSquared, 
 							   ULL halfBase, ULL base, 
-							   ULL* pointsLength, int* outsideRecord) {
+							   ULL nDimensions, int* record) {
 
 	ULL id = blockIdx.x * blockDim.x + threadIdx.x;
-	ULL dimension = threadIdx.y + 1;
 
 	if (id < nPointsToTest) {
-		long dimensionalValue = getDimensionalValue(id, base, dimension);
-		// addComponentToLength(id, dimensionalValue, halfBase, pointsLength);
+		ULL index[MAX_DIMENSIONS];
+		for (int i = 0; i < MAX_DIMENSIONS; i++) {
+			index[i] = halfBase;
+		}
 
-		// long long difference = dimensionalValue - halfBase;
-		// ULL differenceSquared = difference * difference;
-		// atomicAdd(&pointsLength[id], dimensionalValue);
-		// atomicAdd(&outsideRecord[id], dimensionalValue);
+		ULL point = id;
+		ULL distance = 0;
+		for (int i = 0; i < nDimensions; i++) {
+			index[i] = point % base;
+			point = point / base;
 
-		outsideRecord[id] = 5;
+			long long difference = index[i] - halfBase;
+			ULL differenceSquared = (difference * difference);
+			distance += differenceSquared;
+			// record[i] = differenceSquared;
+		}
 
-		// atomicAdd(&pointsLength[id], dimension + id);
+		if (distance < radiusSquared) {
+			record[id] = 1;
+		}
 
 
-		// determineOutside(id, dimension, pointsLength, radiusSquared, outsideRecord);
+
+		// record[id] = id;
 	}
 }
 
 int main(int argc, char** argv) {
+
+	// Test variables that I have the answers to
 	int nTests = 3;
-	long dimensions[] = {1, 2, 3};
+	ULL dimensions[] = {1, 2, 3};
 	double radii[] = {25.5, 2.05, 1.5};
 
+	// Sequential answers
 	runSequentialTestCases(dimensions, radii);
 
+	// Use the 2 dimensional test case if none is selected
 	int testCase = 1;
 
 	if (argc == 2) {
@@ -172,6 +177,7 @@ int main(int argc, char** argv) {
 		print("TestCase is %d", testCase);
 	}
 
+	// initialise important variables
 	const ULL halfBase = static_cast<ULL>(floor(radii[testCase]));
 	const ULL base = 2 * halfBase + 1;
 	const ULL nPointsToTest = powerLong(base, dimensions[testCase]);
@@ -180,61 +186,42 @@ int main(int argc, char** argv) {
 	debug("gpu settings tc:%d: nPointsToTest: %ld, nDimensions: %ld, radius: %.2f, base: %ld", 
 		testCase, nPointsToTest, dimensions[testCase], radii[testCase], base);
 
+	// get the size to transfer to the device and initialise the array that will be sent
 	int nBytesOutsideRecord = sizeof(int) * nPointsToTest;
-	int nBytesPointLength = sizeof(ULL) * nPointsToTest;
-
-	int* outsideRecord = (int *)malloc(nBytesOutsideRecord);
-	ULL* pointsLength = (ULL *)malloc(nBytesPointLength);
+	int* record = (int *)malloc(nBytesOutsideRecord);
 	for (int i = 0; i < nPointsToTest; ++i) {
-		outsideRecord[i] = 0;
-		pointsLength[i] = 0;
+		record[i] = 0;
 	}
 
-	int* gpuOutsideRecord;
-	ULL* gpuPointsLength;
+	int* gpuRecord;
 
-	cudaMalloc(&gpuOutsideRecord, nBytesOutsideRecord);
-	cudaMalloc(&gpuPointsLength, nBytesPointLength);
+	cudaMalloc(&gpuRecord, nBytesOutsideRecord);
 
-	cudaMemcpy(gpuOutsideRecord, outsideRecord, nBytesOutsideRecord, cudaMemcpyHostToDevice);
-	cudaMemcpy(gpuPointsLength, pointsLength, nBytesPointLength, cudaMemcpyHostToDevice);
+	cudaMemcpy(gpuRecord, record, nBytesOutsideRecord, cudaMemcpyHostToDevice);
 
-	int xThreads = 1024/dimensions[testCase];
-	int nThreads = xThreads * dimensions[testCase];
+	int nThreads = 256;
 	int nBlocks = (nPointsToTest + nThreads - 1) / nThreads;
 
-	debug("Threads X: %d, threads Y: %d, totalPerBlock %d", xThreads, dimensions[testCase], nThreads);
-
-	dim3 blockDimensions(nThreads, dimensions[testCase], 1);
+	dim3 blockDimensions(nThreads, 1, 1);
 	dim3 gridDimensions(nBlocks, 1, 1);
 
-	gpuCountPoints<<<gridDimensions, blockDimensions>>>(nPointsToTest, radiusSquared, halfBase, base, gpuPointsLength, gpuOutsideRecord);
+	gpuCountPoints<<<gridDimensions, blockDimensions>>>(nPointsToTest, radiusSquared, halfBase, base, dimensions[testCase], gpuRecord);
 
-	cudaMemcpy(outsideRecord, gpuOutsideRecord, nBytesOutsideRecord, cudaMemcpyDeviceToHost);
-	cudaMemcpy(pointsLength, gpuPointsLength, nBytesPointLength, cudaMemcpyDeviceToHost);
+	cudaMemcpy(record, gpuRecord, nBytesOutsideRecord, cudaMemcpyDeviceToHost);
 
-	cudaFree(gpuOutsideRecord);
-	cudaFree(gpuPointsLength);
+	cudaFree(gpuRecord);
 
 	long count = 0;
 	for (int i = 0; i < nPointsToTest; ++i) {
-		if (outsideRecord[i] == 0) {
+		if (record[i] == 1) {
 			count++;
 		}
 	}
 
-	printArray(pointsLength, nPointsToTest);
+	print("Ok, now for outside record, remember radius^2 is %f", radiusSquared);
+	printArray(record, nPointsToTest);
 
-	print("");
-	print("Ok, now for outside record");
-	// printArray(outsideRecord, nPointsToTest);
-
-	for (int i = 0; i < nPointsToTest; i++) {
-		print("%d :: %lld", i, outsideRecord[i]);
-	}
-
-	free(outsideRecord);
-	free(pointsLength);
+	free(record);
 
 	print("Parallel test %d:", testCase);
 	printTestResults(dimensions[testCase], radii[testCase], count);
